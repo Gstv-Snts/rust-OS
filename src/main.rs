69d0a4bf2e3d1dc0a92a47bf693ca288a96adee3
Gstv-Snts <gustavosantossilveira12360@gmail.com>
2024-07-07 09:57:01 -0300

    vga text mode

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,21 @@
 #![no_std]
-fn main() {}
+#![no_main]
+
+mod vga_buffer;
+
+use core::{fmt::Write, panic::PanicInfo};
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+static HELLO: &[u8] = b"Hello, world!";
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello world{}", "!");
+    panic!("Panic!!!!!!!!!!!!");
+    loop {}
+}
